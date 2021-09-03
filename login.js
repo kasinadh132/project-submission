d2af9cc75ecafa0a5762d60f71e97a1273b090e4
swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadh132@users.noreply.github.com>
2021-09-03 21:02:27 +0530

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,8 @@ const signUpButton = document.getElementById('Sign up');
 const LoginButton = document.getElementById('Login');
 const container = document.getElementById('container');
 
-LoginButton.onclick = function() {
+LoginButton.onclick = function() 
+{
     location.href = 'etc/bypass/index.html';
 }
 
