dab78569ada2a732e0677c18edb476fa46550b44
swayampakula.v.s.s.pavanakasinadhasarma <81065703+kasinadh132@users.noreply.github.com>
2021-09-04 05:57:47 +0530

    .

--- dbconnection.php
+++ dbconnection.php
@@ -1,39 +1,4 @@
-<html>  
-<head lang="en">  
-    <meta charset="UTF-8">  
-    <link type="text/css" rel="stylesheet" href="bootstrap-3.2.0-dist\css\bootstrap.css"> <!--css file link in bootstrap folder-->  
-    <title>View Users</title>  
-</head>  
-<style>  
-    .login-panel {  
-        margin-top: 150px;  
-    }  
-    .table {  
-        margin-top: 50px;  
-     }  
-</style>  
-  
-<body>  
-  
-<div class="table-scrol">  
-    <h1 align="center">All the Users</h1>  
-  
-<div class="table-responsive"><!--this is used for responsive display in mobile and other devices-->  
-    <table class="table table-bordered table-hover table-striped" style="table-layout: fixed">  
-        <thead>  
-  
-        <tr>  
-  
-            <th>User Id</th>  
-            <th>User Name</th>  
-            <th>User E-mail</th>  
-            <th>User Pass</th>  
-            <th>Delete User</th>  
-        </tr>  
-        </thead>  
-  
-        <?php  
-        include("database/db_conection.php");  
+        <?php         include("database/db_conection.php");  
         $view_users_query="select * from users";//select query for viewing users.  
         $run=mysqli_query($dbcon,$view_users_query);//here run the sql query.  
   
@@ -47,7 +12,6 @@
         ?>  
   
         <tr>  
-<!--here showing results in the table -->  
             <td><?php echo $user_id;  ?></td>  
             <td><?php echo $user_name;  ?></td>  
             <td><?php echo $user_email;  ?></td>  
@@ -58,8 +22,3 @@
         <?php } ?>  
   
     </table>  
-        </div>  
-</div>  
-</body>  
-  
-</html>  
--- login.js
+++ login.js
@@ -2,13 +2,6 @@ const signUpButton = document.getElementById('Sign up');
 const LoginButton = document.getElementById('Login');
 const container = document.getElementById('container');
 
-LoginButton.onclick = function() 
-{
+LoginButton.onclick = function() {
     location.href = 'etc/bypass/index.html';
-}
-
-SignUp.onclick = function()
-{
-    location.href = 'etc/bypass/index.html';
-
 }
