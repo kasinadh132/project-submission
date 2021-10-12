9035d2fa7fc3de97015357982048fd01e060f7de
swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadh132@users.noreply.github.com>
2021-10-12 11:13:50 +0530

    /

--- login.js
+++ login.js
@@ -5,3 +5,10 @@ const container = document.getElementById('container');
 LoginButton.onclick = function() {
     location.href = 'index.html';
 }
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb://kasi353l:<password>@datalake0-kzctj.a.query.mongodb.net/myFirstDatabase?ssl=true&authSource=admin";
+MongoClient.connect(uri, function(err, client) {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
